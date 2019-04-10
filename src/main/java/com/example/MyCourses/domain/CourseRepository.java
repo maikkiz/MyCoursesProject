5ee927503701e5e4086574c92e86949871e3fe3d
Maija Hoikka <hoikka.maija@gmail.com>
2019-04-10 11:01:40 +0300

    Adding decimal formatting

--- src/main/java/com/example/MyCourses/domain/CourseRepository.java
+++ src/main/java/com/example/MyCourses/domain/CourseRepository.java
@@ -11,9 +11,11 @@ public interface CourseRepository extends CrudRepository<Course, Long> {
 	// finds courses by name
 	List<Course> findByName(@Param("name") String name);
 
+	// counts the average grade of all courses
 	@Query("SELECT AVG(c.grade) from Course c")
 	double getAverageGrade();
 	
+	// counts the sum on all credits
 	@Query("SELECT SUM(c.credit) from Course c")
 	int getTotalCredit();
 
